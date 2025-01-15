39f9a404ffb86c72eccdd7a3ae8a82f6f16015d7
Aziz Hammmami <mohamedazizhammami01@gmail.com>
2025-01-15 22:01:38 +0100

    first commit

---
+++ test.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+
+function getnumbersfromstring(string) {
+    const numbers = string.match(/[0-9]+/g);
+    if (numbers) {
+        var concatenatedNumber = '';
+        for (var num of numbers) {
+            concatenatedNumber += num;
+        }
+        return Number(concatenatedNumber);
+    }
+    return 0;
+}
+function checkfile(nomfichier) {
+    try {
+        const data = fs.readFileSync(nomfichier, 'utf8');
+        const lines = data.split('\n');
+        let total = 0;
+        for (const line of lines) {
+            const line1 = line.trim();
+            if (line1) {
+                total += getnumbersfromstring(line1);
+            }
+        }
+        return total;
+    } catch (err) {
+        console.log(err.message);
+        return null;
+    }
+}
+const nomfichier = 'C:/Users/bazaz/Downloads/document.txt';
+const result = checkfile(nomfichier);
+if (result !== null) {
+    console.log('La somme totale des nombres est:' + result);
+}
